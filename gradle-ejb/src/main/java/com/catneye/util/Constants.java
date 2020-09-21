48d6d711068a515465582731c230cda4970c3fcd
plintus <olegus28@gmail.com>
2020-09-21 13:05:22 +0300

    Constant class for service

---
+++ gradle-ejb/src/main/java/com/catneye/util/Constants.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.catneye.util;
+
+/**
+ *
+ * @author plintus
+ */
+public interface Constants {
+    public String SERVICE_NAME = "Poduct service";
+    public String SERVICE_BEAN_NAME
+            = "java:global/gradle-ear/gradle-ejb/ProductBean!com.catneye.bean.ProductBeanRemote";
+}
