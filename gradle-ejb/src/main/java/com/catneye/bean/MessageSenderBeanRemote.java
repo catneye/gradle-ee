241263b0f877e6c141977855a11c5e8148598cfc
plintus <olegus28@gmail.com>
2020-09-24 12:46:00 +0300

    New jms functions

---
+++ gradle-ejb/src/main/java/com/catneye/bean/MessageSenderBeanRemote.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.catneye.bean;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author plintus
+ */
+@Remote
+public interface MessageSenderBeanRemote {
+    public void sendTextMessage(String msg);
+}
